6f1c8b427abbcbe9648920cc1d70d5404cc5d184
KVasev <vasev91@gmail.com>
2015-07-01 12:14:46 +0300

    Added Button.cs in UI. Fixed a few things in UIItem.cs and UIManager.cs.

--- Teamwork-OOP/Engine/UI/UIItem.cs
+++ Teamwork-OOP/Engine/UI/UIItem.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using OpenTK.Graphics.ES10;
+using Teamwork_OOP.Engine.Drawing;
 
 namespace Teamwork_OOP.Engine.UI
 {
 	using Physics;
-
+	public delegate void OnClickEventHandler();
 	public abstract class UIItem
 	{
 		private AABB collisionBox;
 
-		public UIItem(Vector2 position, Vector2 size)
+		public UIItem(TextureNode texture, Vector2 position, Vector2 size, string name)
 		{
 			this.collisionBox = new AABB(position, size, CollisionObjectFlags.Kinematic);
+		}
+
+		public AABB CollisionBox
+		{
+			get { return collisionBox; }
+		}
+		public bool isMouseOver { get; set; }
+		public event OnClickEventHandler OnClick;
+
+		public void RaiseClickEvent()
+		{
+			if (OnClick != null)
+			{
+				OnClick();
+			}
 		}
 	}
 }
